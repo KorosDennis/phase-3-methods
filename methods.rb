61cc5d2023fdc53563fc3ec17439a4b8501c53fc
korosdennis <deniptoo98@gmaiil.com>
2022-08-14 12:53:16 +0300

    commit passed all test

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name )
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(a,b)
+    return a+b
+end
+
+def halve(x)
+    if x.class != Integer
+        return nil
+    end
+     (x/2)
+end
